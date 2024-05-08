8922848c6c90c296011df6aa438346d927146098
Isaac Soares <isaacsantos.soares@hotmail.com>
2024-05-08 20:15:35 -0300

    Update matrix_tasks.c

--- FreeRTOS_Matriz/matrix_tasks.c
+++ FreeRTOS_Matriz/matrix_tasks.c
@@ -37,30 +37,13 @@
 #include "matrix.h"
 #include "pontoflutuante.h"
 
-/* Priorities used by the tasks. */
-#define mainQUEUE_RECEIVE_TASK_PRIORITY         ( tskIDLE_PRIORITY + 2 )
-#define mainQUEUE_SEND_TASK_PRIORITY            ( tskIDLE_PRIORITY + 1 )
-
-/* The rate at which data is sent to the queue.  The 500ms value is converted
- * to ticks using the pdMS_TO_TICKS() macro. */
-#define mainQUEUE_SEND_FREQUENCY_MS             pdMS_TO_TICKS( 500 )
-
-/* The maximum number items the queue can hold.  The priority of the receiving
- * task is above the priority of the sending task, so the receiving task will
- * preempt the sending task and remove the queue items each time the sending task
- * writes to the queue.  Therefore the queue will never have more than one item in
- * it at any time, and even with a queue length of 1, the sending task will never
- * find the queue full. */
-#define mainQUEUE_LENGTH                        ( 16 )
-
-
 typedef struct{
     uint32_t linha;
     uint32_t coluna;
 } TaskArgs;
 
 float ** matrix1, ** matrix2, ** matrix3;
-int sacanagemLiberada = 0;
+int liberaPrint = 0;
 
 TaskArgs args[MAX_MATRIX*MAX_MATRIX];
 
@@ -79,15 +62,9 @@ void matrix_tasks( void );
 /**
  * The tasks as described in the comments at the top of this file.
  */
-static void sacanagemLiberando(void * sacanagem);
+static void imprimeMatrizResultante(void * sacanagem);
 static void multiplicaLinhaColuna(void * args);
 
-/*-----------------------------------------------------------*/
-
-/* The queue used by both tasks. */
-static QueueHandle_t xQueue = NULL;
-
-/*-----------------------------------------------------------*/
 void matrix_tasks(void) {
     matrix1 = criarMatrizFloat(MAX_MATRIX);
     instanciaMatrizUnitaria(matrix1, MAX_MATRIX );
@@ -104,29 +81,22 @@ void matrix_tasks(void) {
         }
     }
     
-    //Cria a fila de TASKs.
-    xQueue = xQueueCreate( mainQUEUE_LENGTH, sizeof( unsigned long ) );
-    //Se a fila existe.
-    if( xQueue != NULL )
-    {
-        for(uint32_t i = 0; i < MAX_MATRIX; i++){
-            for(uint32_t j = 0; j < MAX_MATRIX; j++){
-                args[(i*MAX_MATRIX) + j].linha = i;
-                args[(i*MAX_MATRIX) + j].coluna = j;
-                xTaskCreate(multiplicaLinhaColuna, "Linha x Coluna", configMINIMAL_STACK_SIZE, &args[(i*MAX_MATRIX)+j], mainQUEUE_SEND_TASK_PRIORITY, NULL);
-            }
+
+    for(uint32_t i = 0; i < MAX_MATRIX; i++){
+        for(uint32_t j = 0; j < MAX_MATRIX; j++){
+            args[(i*MAX_MATRIX) + j].linha = i;
+            args[(i*MAX_MATRIX) + j].coluna = j;
+            xTaskCreate(multiplicaLinhaColuna, "Linha x Coluna", configMINIMAL_STACK_SIZE, &args[(i*MAX_MATRIX)+j], 0, NULL);
         }
-        xTaskCreate(sacanagemLiberando, "Print Resultado", configMINIMAL_STACK_SIZE, NULL, mainQUEUE_SEND_TASK_PRIORITY, NULL);
-        t_inicio = neorv32_mtime_get_time();
-        vTaskStartScheduler();
     }
+    xTaskCreate(imprimeMatrizResultante, "Print Resultado", configMINIMAL_STACK_SIZE, NULL, 0, NULL);
+    t_inicio = neorv32_mtime_get_time();
+    vTaskStartScheduler();
 
     while(1){
         myPrint("ERRO: memória heap insuficiente!\n");
     };
 }
-
-/*-----------------------------------------------------------*/
 
 static void multiplicaLinhaColuna(void * args){
     uint32_t linha = ((TaskArgs*)args)->linha;
@@ -139,15 +109,15 @@ static void multiplicaLinhaColuna(void * args){
     //região crítica...
     while (mutex);
     mutex = 1;
-        sacanagemLiberada++;
+        liberaPrint++;
     mutex = 0;
 
     vTaskDelete(NULL);
 }
 
-static void sacanagemLiberando(void * sacanagem){
+static void imprimeMatrizResultante(void * sacanagem){
     (void) sacanagem;
-    while(sacanagemLiberada < MAX_MATRIX * MAX_MATRIX);
+    while(liberaPrint < MAX_MATRIX * MAX_MATRIX);
     t_fim = neorv32_mtime_get_time();
     imprimirMatrizFloat(matrix3, MAX_MATRIX);
     longPrint("TEMPO HARDWARE: ", ((double)(t_fim - t_inicio))/50000000);
